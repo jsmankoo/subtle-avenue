a026610304db88ebd3bacc269078da9983fee03d
jsmankoo <jsmankoo@outlook.com>
2016-04-03 00:11:26 -0400

    Jai Map almost done

--- app/App.js
+++ app/App.js
@@ -1,16 +1,41 @@
-import React from 'react';
-import styles from './App.css';
+import React, {createClass} from 'react';
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {test: 'foo'};
-  }
-  render() {
+import styles from './Stylus/app.styl';
+
+import Nav from './Components/Nav';
+import MapWrapper from './Components/MapWrapper';
+import Overlay from './Components/Overlay';
+
+const App = createClass({
+  getInitialState(){
+    return {
+      windowHeight: $(window).height(),
+      dayOfWeek: 1,
+      hourOfDay: 0,
+      TrafficData: [],
+      uberData:[]
+    };
+  },
+  componentDidMount(){
+    $(window).resize(()=>this.setState({...this.state,
+      windowHeight: $(window).height()
+    }));
+    // var socket = io('http://localhost:3000/');
+    // socket.on('news', function (data) {
+    //   console.log(data);
+    //   socket.emit('my other event', { my: 'data' });
+    // });
+  },
+  render(){
+    const {windowHeight} = this.state;
     return (
-      <div className={styles.app}>
-        Hello foo
+      <div className='App'>
+        <Nav />
+        <MapWrapper height={windowHeight - 61} />
+        <Overlay />
       </div>
     );
   }
-}
+});
+
+module.exports = App;
